d8b53ed0e1056f1ff39d71b9bc9cdcb86c5d98c4
peppyeben <austineben34@gmail.com>
2024-11-01 05:38:36 +0100

    NextJS Migration | 70% of Vanilla React features

--- src/components/WalletWrapper.tsx
+++ src/components/WalletWrapper.tsx
@@ -1,57 +1,61 @@
-'use client';
+"use client";
 import {
-  Address,
-  Avatar,
-  EthBalance,
-  Identity,
-  Name,
-} from '@coinbase/onchainkit/identity';
+    Address,
+    Avatar,
+    EthBalance,
+    Identity,
+    Name,
+} from "@coinbase/onchainkit/identity";
 import {
-  ConnectWallet,
-  Wallet,
-  WalletDropdown,
-  WalletDropdownBasename,
-  WalletDropdownDisconnect,
-  WalletDropdownFundLink,
-  WalletDropdownLink,
-} from '@coinbase/onchainkit/wallet';
+    ConnectWallet,
+    Wallet,
+    WalletDropdown,
+    WalletDropdownBasename,
+    WalletDropdownDisconnect,
+    WalletDropdownFundLink,
+    WalletDropdownLink,
+} from "@coinbase/onchainkit/wallet";
 
 type WalletWrapperParams = {
-  text?: string;
-  className?: string;
-  withWalletAggregator?: boolean;
+    text?: string;
+    className?: string;
+    withWalletAggregator?: boolean;
 };
 export default function WalletWrapper({
-  className,
-  text,
-  withWalletAggregator = false,
+    className,
+    text,
+    withWalletAggregator = false,
 }: WalletWrapperParams) {
-  return (
-    <>
-      <Wallet>
-        <ConnectWallet
-          withWalletAggregator={withWalletAggregator}
-          text={text}
-          className={className}
-        >
-          <Avatar className="h-6 w-6" />
-          <Name />
-        </ConnectWallet>
-        <WalletDropdown>
-          <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick={true}>
-            <Avatar />
-            <Name />
-            <Address />
-            <EthBalance />
-          </Identity>
-          <WalletDropdownBasename />
-          <WalletDropdownLink icon="wallet" href="https://wallet.coinbase.com">
-            Go to Wallet Dashboard
-          </WalletDropdownLink>
-          <WalletDropdownFundLink />
-          <WalletDropdownDisconnect />
-        </WalletDropdown>
-      </Wallet>
-    </>
-  );
+    return (
+        <>
+            <Wallet>
+                <ConnectWallet
+                    withWalletAggregator={withWalletAggregator}
+                    text={text}
+                    className={className}
+                >
+                    <Avatar className="h-6 w-6 bg-white stroke-white" />
+                    <Name className="text-white"/>
+                </ConnectWallet>
+                <WalletDropdown>
+                    <Identity
+                        className="px-4 pt-3 pb-2"
+                        hasCopyAddressOnClick={true}
+                    >
+                        <Avatar />
+                        {/* <Name /> */}
+                        <Address />
+                        <EthBalance />
+                    </Identity>
+                    {/* <WalletDropdownLink
+                        icon="wallet"
+                        href="https://wallet.coinbase.com"
+                    >
+                        Go to Wallet Dashboard
+                    </WalletDropdownLink> */}
+                    <WalletDropdownDisconnect />
+                </WalletDropdown>
+            </Wallet>
+        </>
+    );
 }
